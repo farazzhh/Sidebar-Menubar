6c94565ec073199f15dbbd73cb1486d6823d52ca
farazzhh <havaee.faraz@gmail.com>
2021-07-01 03:16:28 +0430

    add jquery to load page content without page refreshing

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+
+// sidebar show & hide function
 const showMenu = (toggleId, navbarId, bodyId) => {
     const toggle = document.getElementById(toggleId),
         navbar = document.getElementById(navbarId),
@@ -14,4 +17,32 @@ const showMenu = (toggleId, navbarId, bodyId) => {
 
 showMenu('nav-toggle', 'navbar', 'body')
 
-const linkColor = document.querySelectorAll('.nav__link');
+
+
+// get content of page and put in mainSection element without page refreshing
+    const linkColor = document.querySelectorAll('.nav__link');
+    
+    linkColor.forEach(link => {
+        link.addEventListener('click', () => {
+            const pageName = link.lastElementChild.innerText;
+            
+            if (!link.classList.contains("active")) {
+                document.querySelector(".active").classList.remove("active");
+                link.classList.add("active");
+            }
+            
+            // use Jquery to grab specific page and put in mainSection element(body)
+            $("#mainsection").load(`pages/${pageName}.html`);
+        })
+    })
+    
+
+
+// set default function 
+const default_function = () => {
+    $("#mainsection").load("pages/home.html");
+}
+
+
+//  (page = Home) when index.html loaded for first time
+window.addEventListener("DOMContentLoaded", default_function );
